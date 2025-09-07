3ec4b026055119021a096f69ad25c1907901c006
Gaspar Acevedo <acevedo.zain.gaspar@gmail.com>
2025-09-06 21:13:27 -0300

    [TP-01] Estructura de carpetas + WIP de parte 2.

---
+++ resolucion_tps/utilities/graficos.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+
+def graficar_imagen_e_histograma(imagen, histograma,
+
+                                 cmap: any = "gray",
+                                 vmin: float = 0,
+                                 vmax: float = 255,
+                                 title: str = "Imagen e histograma"):
+    fig = plt.figure()
+    ax1 = plt.subplot(211)
+    ax1.imshow(imagen, cmap=cmap, vmin=vmin, vmax=vmax)
+    ax1.set_title(title)
+
+    ax2 = plt.subplot(212)
+    ax2.plot(histograma)
+
+def graficar_2_histogramas(histograma1, histograma2,
+                         title1: str,
+                         title2: str):
+    fig = plt.figure()
+    ax1 = plt.subplot(211)
+    ax1.plot(histograma1)
+    ax1.set_title(title1)
+
+    ax2 = plt.subplot(212)
+    ax2.plot(histograma2)
+    ax2.set_title(title2)
